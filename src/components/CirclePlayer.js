e595dac7f4b055918cd22d89af14d216b06f62d5
Paolo <str237@gmail.com>
2021-07-03 19:12:34 +0200

    Hook: useInit

--- src/components/CirclePlayer.js
+++ src/components/CirclePlayer.js
@@ -1,12 +1,22 @@
-import React, { useRef } from 'react'
+import React, { useEffect, useRef } from 'react'
 import { useFrame } from "@react-three/fiber";
 import { Circle } from "./Circle";
+import { ZERO3 } from "../constants";
+
+const useInit = (circle) => {
+  useEffect(() => {
+    circle.current.speed = ZERO3
+    circle.current.acceleration = ZERO3
+  }, [circle])
+}
 
 export const CirclePlayer = ({ target, ...props }) => {
   const circle = useRef()
   useFrame((rootState) => {
-    circle.current.position.lerp(target, 0.05)
+    const c = circle.current
+    c.position.lerp(target, 0.05)
   })
+  useInit(circle)
   return (
     <Circle {...props}
             ref={circle}
